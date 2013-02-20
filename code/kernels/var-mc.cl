5daca8a721bf5ebf25a820d0961f150944690220
Paraita Wohler <milkyboi@gmail.com>
2013-02-20 15:22:23 +0100

    nettoyage

---
+++ code/kernels/var-mc.cl
@@ -0,0 +1,25 @@
+__kernel void calcul_trajectoires(__global const float *RENDEMENTS,
+				  __global const float *VOLS,
+				  __global const float *TI,
+				  __global float *TIRAGES,
+				  __global int *nb_actions,
+				  __global int *horizon) {
+  int i = get_global_id(0);
+  TIRAGES[i] = 0;
+  float tmp;
+  for(int a=0; a < (*nb_actions);a++) {
+    tmp = PORTEFEUILLE[a];
+    for(int t=0; t < (*horizon);t++) {
+      int index = i * (*nb_actions);
+      index += a * (*horizon);
+      index += t;
+      tmp = tmp*ALEA[index];
+    }
+    TIRAGES[i]+=tmp;
+  }
+  tmp = 0;
+  for(int j=0; j < (*nb_actions); j++) {
+    tmp += PORTEFEUILLE[j];
+  }
+  TIRAGES[i]+=tmp;
+}
--- code/src/main.cpp
+++ code/src/main.cpp
@@ -23,133 +23,33 @@
 #define PRINT_USAGE "Usage: -c seuil_confiance -n nb_tirages -p portefeuille -t horizon"
 
 
-bool parse_args(int argc,char** argv,float* seuil_confiance,int* nb_tirages,int* p,int* horizon);
-void calcul(float seuil_confiance,int nb_tirages,int portefeuille,int T);
+bool parse_args(int argc,char** argv,float* seuil_confiance,int* nb_tirages,std::string* p,int* horizon);
+void calcul(float seuil_confiance,int nb_tirages,std::string portefeuille,int T);
 
 int main(int argc, char *argv[])
 {
   bool param_ok = false;
   float seuil_confiance = 0;
   int nb_tirages = 0;
-  int portefeuille = 0;
+  std::string portefeuille;
   int horizon = 0;
   
   // verif des parametres
   param_ok = parse_args(argc, argv, &seuil_confiance, &nb_tirages, &portefeuille, &horizon);
   
-  if (param_ok) {
-    printf("Parametres:\n\tseuil de confiance:%f\n\tnb tirages:%d\n\thorizon:%d\n",
-	   seuil_confiance,nb_tirages,horizon);
-    calcul(seuil_confiance, nb_tirages, portefeuille, horizon);
-    return EXIT_SUCCESS;
-  }
-  else {
-    std::cout << PRINT_USAGE << std::endl;
-    return EXIT_FAILURE;
-  }
+  // if (param_ok) {
+  //   printf("Parametres:\n\tseuil de confiance:%f\n\tnb tirages:%d\n\thorizon:%d\n\tchemin portefeuille:%s\n",
+  // 	   seuil_confiance,nb_tirages,horizon, portefeuille.c_str());
+  //   calcul(seuil_confiance, nb_tirages, portefeuille, horizon);
+  //   return EXIT_SUCCESS;
+  // }
+  // else {
+  //   std::cout << PRINT_USAGE << std::endl;
+  //   return EXIT_FAILURE;
+  // }
 }
 
-bool parse_args(int argc,
-		char** argv,
-		float* seuil_confiance,
-		int* nb_tirages,
-		int* p,
-		int* horizon)
-{
-  // affiche l'aide et le hardware dispo sur la machine
-  if (argc == 2 && (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "-help") == 0)) {
-    CLManager clm;
-    std::cout << "Plateformes disponibles:" << std::endl;
-    std::cout << clm.printPlatform() << std::endl;
-    return false;
-  }
-  else {
-    if (argc != 9) {
-      std::cout << "\tpas le bon nombre de params ! (" << argc << ")" << std::endl;
-      return false;
-    }
-    // sinon on recupere les parametres
-    else {
-      bool _seuil_confiance_ok = false;
-      bool _nb_tirages_ok = false;
-      bool _p_ok = false;
-      bool _horizon_ok = false;
-      for (int i = 1; i < argc; i++) {
-	// parametre: seuil de confiance
-	if (strcmp(argv[i], "-c") == 0) {
-	  if (i == argc-1 || _seuil_confiance_ok) {
-	    std::cout << "\terreur seuil de confiance" << std::endl;
-	    return false;
-	  }
-	  else {
-	    *seuil_confiance = atof(argv[++i]);
-	    _seuil_confiance_ok = true;
-	    continue;
-	  }
-	}
-	// parametre: nombre de tirages
-	if (strcmp(argv[i], "-n") == 0) {
-	  if (i == argc-1 || _nb_tirages_ok) {
-	    std::cout << "\terreur nombre de tirages" << std::endl;
-	    return false;
-	  }
-	  else {
-	    *nb_tirages = atoi(argv[++i]);
-	    _nb_tirages_ok = true;
-	    continue;
-	  }
-	}
-	// parametre: portefeuille
-	if (strcmp(argv[i], "-p") == 0) {
-	  if (i == argc-1 || _p_ok) {
-	    std::cout << "\terreur portefeuille" << std::endl;
-	    return false;
-	  }
-	  else {
-	    *p = atoi(argv[++i]);
-	    _p_ok = true;
-	    continue;
-	  }
-	}
-	// parametre: horizon
-	if (strcmp(argv[i], "-t") == 0) {
-	  if (i == argc-1 || _horizon_ok) {
-	    std::cout << "\terreur horizon" << std::endl;
-	    return false;
-	  }
-	  else {
-	    *horizon = atoi(argv[++i]);
-	    _horizon_ok = true;
-	    continue;
-	  }
-	}
-      }
-      // est ce qu'on a tout les parametres ?
-      if (!_seuil_confiance_ok ||
-	  !_nb_tirages_ok ||
-	  !_p_ok ||
-	  !_horizon_ok) {
-	std::cout << "\terreur on a pas tout les parametres" << std::endl;
-	return false;
-      }
-      else {
-	// est ce que les parametres sont valides ?
-	if ( *seuil_confiance < 1 &&
-	     *seuil_confiance > 0 &&
-	     *nb_tirages > 0 &&
-	     *horizon > 0) {
-	  return true;
-	}
-	else {
-	  std::cout << "\terreur parametre(s) non valide(s)" << std::endl;
-	  return false;
-	}
-      }
-    }
-  }
-}
-
-void calcul(float seuil_confiance, int nb_tirages, int portefeuille, int T) {
+void calcul(float seuil_confiance, int nb_tirages, std::string portefeuille, int T) {
   CLManager clm;
   std::cout << clm.printPlatform();
   clm.init(0,0);
@@ -159,7 +59,7 @@ void calcul(float seuil_confiance, int nb_tirages, int portefeuille, int T) {
   int NB_ACTIONS = 10;
   float *N = (float *) calloc(NB_ACTIONS * nb_tirages * T, sizeof(float));
   float *TIRAGES = (float *) calloc(nb_tirages, sizeof(float));
-
+  
   float P[NB_ACTIONS];
   P[0] = 123.0;
   P[1] = 99.0;
@@ -223,6 +123,106 @@ void calcul(float seuil_confiance, int nb_tirages, int portefeuille, int T) {
 
 
 
+
+bool parse_args(int argc,
+		char** argv,
+		float* seuil_confiance,
+		int* nb_tirages,
+		std::string* p,
+		int* horizon)
+{
+  // affiche l'aide et le hardware dispo sur la machine
+  if (argc == 2 && (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "-help") == 0)) {
+    CLManager clm;
+    std::cout << "Plateformes disponibles:" << std::endl;
+    std::cout << clm.printPlatform() << std::endl;
+    return false;
+  }
+  else {
+    if (argc != 9) {
+      std::cout << "\tpas le bon nombre de params ! (" << argc << ")" << std::endl;
+      return false;
+    }
+    // sinon on recupere les parametres
+    else {
+      bool _seuil_confiance_ok = false;
+      bool _nb_tirages_ok = false;
+      bool _p_ok = false;
+      bool _horizon_ok = false;
+      for (int i = 1; i < argc; i++) {
+	// parametre: seuil de confiance
+	if (strcmp(argv[i], "-c") == 0) {
+	  if (i == argc-1 || _seuil_confiance_ok) {
+	    std::cout << "\terreur seuil de confiance" << std::endl;
+	    return false;
+	  }
+	  else {
+	    *seuil_confiance = atof(argv[++i]);
+	    _seuil_confiance_ok = true;
+	    continue;
+	  }
+	}
+	// parametre: nombre de tirages
+	if (strcmp(argv[i], "-n") == 0) {
+	  if (i == argc-1 || _nb_tirages_ok) {
+	    std::cout << "\terreur nombre de tirages" << std::endl;
+	    return false;
+	  }
+	  else {
+	    *nb_tirages = atoi(argv[++i]);
+	    _nb_tirages_ok = true;
+	    continue;
+	  }
+	}
+	// parametre: portefeuille
+	if (strcmp(argv[i], "-p") == 0) {
+	  if (i == argc-1 || _p_ok) {
+	    std::cout << "\terreur portefeuille" << std::endl;
+	    return false;
+	  }
+	  else {
+	    *p = argv[++i];
+	    _p_ok = true;
+	    continue;
+	  }
+	}
+	// parametre: horizon
+	if (strcmp(argv[i], "-t") == 0) {
+	  if (i == argc-1 || _horizon_ok) {
+	    std::cout << "\terreur horizon" << std::endl;
+	    return false;
+	  }
+	  else {
+	    *horizon = atoi(argv[++i]);
+	    _horizon_ok = true;
+	    continue;
+	  }
+	}
+      }
+      // est ce qu'on a tout les parametres ?
+      if (!_seuil_confiance_ok ||
+	  !_nb_tirages_ok ||
+	  !_p_ok ||
+	  !_horizon_ok) {
+	std::cout << "\terreur on a pas tout les parametres" << std::endl;
+	return false;
+      }
+      else {
+	// est ce que les parametres sont valides ?
+	if ( *seuil_confiance < 1 &&
+	     *seuil_confiance > 0 &&
+	     *nb_tirages > 0 &&
+	     *horizon > 0) {
+	  return true;
+	}
+	else {
+	  std::cout << "\terreur parametre(s) non valide(s)" << std::endl;
+	  return false;
+	}
+      }
+    }
+  }
+}
 
 
 
