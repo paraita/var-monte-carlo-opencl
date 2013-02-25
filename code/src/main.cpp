f5dab69663b546f16f00541d19bd2bf32b95b825
Paraita Wohler <milkyboi@gmail.com>
2013-02-25 17:34:13 +0100

    correction du merge chez moi

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -21,7 +21,7 @@
 #include "Portefeuille.h"
 #include <boost/random.hpp>
 #include <boost/random/normal_distribution.hpp>
-//#include <boost/chrono.hpp>
+#include <boost/chrono.hpp>
 #define PRINT_USAGE "Usage: -c seuil_confiance -n nb_tirages -p portefeuille -t horizon [-b]"
 
 
@@ -30,17 +30,6 @@ bool parse_args(int argc,char** argv,float* seuil_confiance,int* nb_tirages,std:
 void calcul1(float seuil_confiance,int nb_tirages,std::string portefeuille,int T,bool debug);
 // RNG sur GPU
 void calcul2();
-
-void calculVariance(	float *TIRAGES,
-			int *nb_Simulation,
-			int *nb_value_par_thread,
-			float *esperance,
-			float *variance,
-			int *nb_THREAD,  
-	      		float *intervalConfiance,      	 
-			float *CARRE,
-			float *ESPERANCE );
-
 
 void calculVariance(	float *TIRAGES,
 			int *nb_Simulation,
@@ -83,13 +72,6 @@ int main(int argc, char *argv[])
   }
 }
 
-<<<<<<< HEAD
-
-void calcul2() {
-||||||| merged common ancestors
-void calcul2(int nb_rn) {
-=======
-
 void calculVariance(	float *TIRAGES,
 			int *nb_Simulation,
 			int *nb_value_par_thread,
@@ -126,10 +108,8 @@ CLManager clm;
 
 
 
-void calcul2(int nb_rn) {
->>>>>>> 07da01a4d4132f05724815456ec5996f512a56d1
+void calcul2() {
   CLManager clm;
-<<<<<<< HEAD
   clm.init(0,1,ENABLE_PROFILING);
   int nb_tirages = 98304;
   int ul_nb_tirages = 98304;
@@ -144,51 +124,6 @@ void calcul2(int nb_rn) {
   clm.executeKernel(nb_tirages, nom_kernel);
   clm.getResultat();
   std::cout << "Nombre de tirages out: " << acc << std::endl;
-}
-
-void calculVariance(	float *TIRAGES,
-			int *nb_Simulation,
-			int *nb_value_par_thread,
-			float *esperance,
-			float *variance,
-			int *nb_THREAD,  
-	      		float *intervalConfiance,      	 
-			float *CARRE,
-			float *ESPERANCE )
-{
-  std::cout << "on attaque la variance"<<std::endl; 
- std::cout << "on a "<< *esperance << std::endl;
-CLManager clm;
-  std::string nom_kernel("calcul_variance");
-  clm.init(0,1,ENABLE_PROFILING);
-  clm.loadKernels("/home/paittaha/var-monte-carlo-opencl/code/kernels/outil.cl");
-  clm.compileKernel(nom_kernel);
-  clm.setKernelArg(nom_kernel, 0, *nb_Simulation,sizeof(float), TIRAGES,false);
-  clm.setKernelArg(nom_kernel, 1, 1, sizeof(int), nb_Simulation, false);
-  clm.setKernelArg(nom_kernel, 2, 1, sizeof(int), nb_value_par_thread, false);
-  clm.setKernelArg(nom_kernel, 3, 1, sizeof(float), esperance, true); // sortie
-  clm.setKernelArg(nom_kernel, 4, 1, sizeof(float), variance, true);  // sortie
-  clm.setKernelArg(nom_kernel, 5, 1, sizeof(int), nb_THREAD, false);
-  clm.setKernelArg(nom_kernel, 6, 1, sizeof(float), intervalConfiance, false);
-  clm.setKernelArg(nom_kernel, 7, *nb_THREAD,sizeof(float), CARRE,false);
-  clm.setKernelArg(nom_kernel, 8, *nb_THREAD,sizeof(float), ESPERANCE,false);
-  // run sur le GPU
-  clm.executeKernel(*nb_Simulation, nom_kernel);
-  // recuperation des résultats
-  clm.getResultat();
-  // on calcul la variance
-  std::cout << "fin du calcul " << std::endl;
-||||||| merged common ancestors
-  clm.init(0,0,ENABLE_PROFILING);
-  clm.loadKernel("var-mc.cl");
-  clm.compileKernel("???");
-  
-=======
-  clm.init(0,0,ENABLE_PROFILING);
-  clm.loadKernels("var-mc.cl");
-  clm.compileKernel("???");
-  
->>>>>>> 07da01a4d4132f05724815456ec5996f512a56d1
 }
 
 void calcul1(float seuil_confiance,
@@ -223,7 +158,7 @@ void calcul1(float seuil_confiance,
   }
 
   // ~~~~~~~~~~~~~~~~~~~~~~ RNG ~~~~~~~~~~~~~~~~~~~~~~~
- // boost::chrono::high_resolution_clock::time_point start_rng = boost::chrono::high_resolution_clock::now();
+  boost::chrono::high_resolution_clock::time_point start_rng = boost::chrono::high_resolution_clock::now();
   float *N = (float *) calloc(NB_ACTIONS * nb_tirages * T, sizeof(float));
   float *TIRAGES = (float *) calloc(nb_tirages, sizeof(float));
   boost::mt19937 rng;
@@ -232,13 +167,13 @@ void calcul1(float seuil_confiance,
   for(int g = 0; g < NB_ACTIONS * nb_tirages * T; g++) {
     N[g] = var_nor();
   }
- // boost::chrono::nanoseconds ns_rng = boost::chrono::high_resolution_clock::now() - start_rng;
+  boost::chrono::nanoseconds ns_rng = boost::chrono::high_resolution_clock::now() - start_rng;
   // ~~~~~~~~~~~~~~~~~~~~~ OpenCL ~~~~~~~~~~~~~~~~~~~~~
   CLManager clm;
   std::string nom_kernel("calcul_trajectoires");
   clm.init(0,1,ENABLE_PROFILING);
-  clm.loadKernels("/home/paittaha/var-monte-carlo-opencl/code/kernels/var-mc.cl");
-  //clm.loadKernels("kernels/var-mc.cl");
+  //clm.loadKernels("/home/paittaha/var-monte-carlo-opencl/code/kernels/var-mc.cl");
+  clm.loadKernels("kernels/var-mc.cl");
   clm.compileKernel(nom_kernel);
   clm.setKernelArg(nom_kernel, 0, NB_ACTIONS, sizeof(float), RENDEMENTS,false);
   clm.setKernelArg(nom_kernel, 1, NB_ACTIONS, sizeof(float), VOLS, false);
@@ -263,21 +198,21 @@ void calcul1(float seuil_confiance,
   // fin calcul de variance 
 
   // ~~~~~~~~~~~~~~ post-traitement VaR ~~~~~~~~~~~~~~~
- // boost::chrono::high_resolution_clock::time_point start_sort = boost::chrono::high_resolution_clock::now();
+  boost::chrono::high_resolution_clock::time_point start_sort = boost::chrono::high_resolution_clock::now();
   std::sort(TIRAGES, TIRAGES+nb_tirages);
- // boost::chrono::nanoseconds ns_sort = boost::chrono::high_resolution_clock::now() - start_sort;
+  boost::chrono::nanoseconds ns_sort = boost::chrono::high_resolution_clock::now() - start_sort;
   
   int percentile = nb_tirages * int(1.0 - seuil_confiance);
   // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- // float t_rng = ns_rng.count() / 1000000.0;
- // float t_sort = ns_sort.count() / 1000000.0;
+  float t_rng = ns_rng.count() / 1000000.0;
+  float t_sort = ns_sort.count() / 1000000.0;
   std::cout << nb_tirages << ";";
   std::cout << P.getRendement() << ";";
   std::cout << TIRAGES[percentile+1] << ";";
   std::cout << NB_ACTIONS * nb_tirages * T * sizeof(float) << ";";
- // std::cout << t_rng << ";";
+  std::cout << t_rng << ";";
   std::cout << clm.getGpuTime() << ";";
- // std::cout << t_sort << std::endl;
+  std::cout << t_sort << std::endl;
 }
 
 bool parse_args(int argc,
